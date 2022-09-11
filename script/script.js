15652f60279d9fdea704912817991396a0a1ab3e
Adrien <adrien.biz@gmail.com>
2022-09-11 14:33:49 +0400

    Feat: toggle read/not-read class on click

--- script/script.js
+++ script/script.js
@@ -1,6 +1,18 @@
+const statusBtns = document.querySelectorAll('.status');
 const deleteBtns = document.querySelectorAll('.delete');
 
+statusBtns.forEach(statusBtn => statusBtn.addEventListener('click', changeStatus))
 deleteBtns.forEach(deleteBtn => deleteBtn.addEventListener('click', removeBook));
+
+function changeStatus(e) {
+    if (e.currentTarget.classList.contains('not-read')) {
+        e.currentTarget.classList.remove('not-read');
+        e.currentTarget.classList.add('read');
+    } else {
+        e.currentTarget.classList.remove('read');
+        e.currentTarget.classList.add('not-read');
+    }
+}
 
 function removeBook(e) {
     e.currentTarget.parentElement.remove();
